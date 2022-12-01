d34f24330f6b637ff65d67b281a7236615cd07bd
Karzan Botani <karzan@botani.nu>
2022-12-02 00:51:37 +0100

    refactor: swapped file type for Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  svelteAllowShorthand: true,
+  svelteIndentScriptAndStyle: false,
+  svelteSortOrder: 'options-scripts-markup-styles',
+  svelteStrictMode: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
