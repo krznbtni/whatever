665701816187552b147978ddb04b9691f54d6d6f
Karzan Botani <karzan@botani.nu>
2022-12-02 01:23:45 +0100

    chore: formatting, eslint rules, etc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,52 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:svelte/recommended',
+    'plugin:svelte/prettier',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.cjs', 'vite.config.js', 'svelte.config.js'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  settings: {
+    svelte: {
+      ignoreWarnings: ['svelte/no-at-html-tags'],
+    },
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    // * JavaScript
+    'arrow-body-style': ['error'],
+    'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+    'no-unused-expressions': ['error'],
+    'prefer-arrow-callback': ['error'],
+    'no-unused-expressions': ['off'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    // * TypeScript
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
